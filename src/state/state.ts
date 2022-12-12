ebdaf08c6c399aec56aa25c75415104dd1258996
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-12-12 11:59:42 +0300

    fake state created

---
+++ src/state/state.ts
@@ -0,0 +1,27 @@
+
+const state = {
+    dialogsPage: {
+        userDialogData: [
+            {id: 1, name: "Oleg"},
+            {id: 2, name: "Viktor"},
+            {id: 3, name: "Onotole"},
+        ],
+        messageItemData: [
+            {id: 1, message: "What is your name???"},
+            {id: 2, message: "What  name???"},
+            {id: 3, message: "What is your /////////"},
+            {id: 4, message: "some some  name???"},
+            {id: 5, message: "..........your name???"},
+        ],
+    },
+    profilePage: {
+        dataPosts: [
+            {id: 1, message: "Yo my friend", likesCount: 34},
+            {id: 2, message: "Yo miend", likesCount: 34},
+            {id: 3, message: "Yo m.......y friend", likesCount: 34},
+            {id: 4, message: "Yo my fr!!!!!!!!!!!!!d", likesCount: 34}
+        ]
+    },
+}
+
+export default state;
